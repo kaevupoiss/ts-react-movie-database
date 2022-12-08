53a4e257d82243fea1740125aafca23a5304bd3b
Rasmus Talioja <rasmus.talioja@gmail.com>
2022-12-09 00:57:35 +0200

    All requirements completed

--- README.md
+++ README.md
@@ -1,3 +1,71 @@
+# Workflow and Timekeeping
+
+## 1. Reading project requirements and planning
+
+The project requirements were straightforward. The design was interesting, yet simple.
+I found some sizes and spacings in the design that I thought were a bit off,
+but I was still going to follow the design as closely as possible.
+
+### 0.5h
+
+## 2. Getting familiar with React and its ecosystem.
+
+### 2h
+
+I have never used React with TypeScript before.
+In fact, I have tried React only once, a couple of years ago, when choosing between React, Vue and Svelte.
+At that time, I chose Vue because I found React unnecessarily verbose and complex.
+It's now time to give React another try.
+
+## 3. Setting up the project
+
+### 1h
+
+I have been following React's developments for a few years.
+I recall some discourse about using `create-react-app` or its alternatives, so I was not sure which one to use for this
+assignment.
+
+Next.js is a framework similar to Nuxt.js, which I have used before in my hobby projects.
+But the scale of this project is small, so I decided to use `create-react-app` with the TypeScript template instead,
+which the official React documentation still recommends.
+I contemplated using Vite, but it requires a bit more configuration, and I wanted to get started painlessly.
+
+In this stage I also added sass by running `npm install sass`, google fonts and material icons.
+
+## 4. Implementing the UI
+
+### 9h
+
+Most of the UI was straightforward to implement, while quickly learning react on the way.
+I decided to use functional components,
+as they resemble Vue's Composition API components, which I am familiar with.
+
+The hardest part of this assignment was creating the movie info tab. I wanted every part of the movie list to be
+responsive.
+I used CSS Grid for the main list. This meant that I had to figure out how to put the movie info exactly between grid
+items depending on its height. For this, I used JavaScript to calculate the height of the movie info, and use it as the
+selected movie poster's height, to push the next row of movies lower. This took the most time to implement.
+
+## 5. Integrating the API
+
+### 3h
+
+I added an environment variable to the `.env` file to store the API Key and the API URL.
+
+While integrating the API, I found an error in the requirements. I had to show popular movies sorted by ratings.
+But I also wanted to implement the optional requirement about loading more items dynamically. I could not have both.
+
+I opted for not sorting by rating, as I implemented dynamic loading. Had I not implemented infinite scroll,
+it would have been a simple addition.
+
+## 6. Refactoring
+
+## 7. Testing
+
+## 8. Summary and Afterthoughts
+
+---
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
@@ -17,7 +85,8 @@ You will also see any lint errors in the console.
 ### `npm test`
 
 Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more
+information.
 
 ### `npm run build`
 
@@ -33,14 +102,20 @@ See the section about [deployment](https://facebook.github.io/create-react-app/d
 
 **Note: this is a one-way operation. Once you `eject`, you can’t go back!**
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will
+remove the single build dependency from your project.
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right
+into your project so you have full control over them. All of the commands except `eject` will still work, but they will
+point to the copied scripts so you can tweak them. At this point you’re on your own.
 
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you
+shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t
+customize it when you are ready for it.
 
 ## Learn More
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+You can learn more in
+the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
 To learn React, check out the [React documentation](https://reactjs.org/).
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,217 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import './App.scss';
+import {useIsVisible} from "./hooks/isVisible";
+
+type Genre = {
+    id: number,
+    name: string
+}
+
+type Error = {
+    status_message: string,
+    status_code: number
+}
+
+type Movie = {
+    poster_path: string,
+    adult: boolean,
+    overview: string,
+    release_date: string,
+    genre_ids: number[],
+    id: number,
+    original_title: string,
+    original_language: string,
+    title: string,
+    backdrop_path: string,
+    popularity: number,
+    vote_count: number,
+    video: boolean,
+    vote_average: number
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [error, setError] = useState<Error | null>(null);
+    const [isLoaded, setIsLoaded] = useState<boolean>(false);
+    const [genres, setGenres] = useState<Genre[]>([])
+    const [showAllGenres, setShowAllGenres] = useState<boolean>(false);
+    const [selectedGenre, setSelectedGenre] = useState<number>(-1)
+    const [movies, setMovies] = useState<Movie[]>([])
+    const [filteredMovies, setFilteredMovies] = useState<Movie[]>([])
+    const [moviesPage, setMoviesPage] = useState<number>(1);
+    const [selectedMovie, setSelectedMovie] = useState<number | null>(null);
+    const [lastMovie, setLastMovie] = useState<HTMLLIElement | null>(null);
+    const [searchQuery, setSearchQuery] = useState<string>('');
+
+    const loadTriggerRef = React.createRef<HTMLLIElement>();
+    const isLoadTriggerVisible = useIsVisible(loadTriggerRef);
+
+    const getGenres = () => {
+        fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + process.env.REACT_APP_TMDB_API_KEY)
+            .then(response => response.json())
+            .then(
+                (result) => {
+                    result.genres.unshift({id: -1, name: 'All Movies'});
+                    setIsLoaded(true);
+                    setGenres(result.genres);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }
+
+    const getMovies = () => {
+
+        fetch('https://api.themoviedb.org/3/discover/movie?api_key=' + process.env.REACT_APP_TMDB_API_KEY + '&page=' + moviesPage)
+            .then(response => response.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setMovies([...movies, ...result.results]);
+                    setGenre(selectedGenre);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            );
+
+        setMoviesPage(moviesPage + 1);
+    }
+
+    const setGenre = (id: number) => {
+        if (id === -1) {
+            setSelectedGenre(-1);
+            setFilteredMovies(movies);
+            return;
+        }
+
+        setSelectedGenre(id);
+        setFilteredMovies(movies.filter(movie => movie.genre_ids.includes(id)));
+    }
+
+    const setMovie = (e: React.MouseEvent<HTMLLIElement>, id: number) => {
+        if (!e.currentTarget) return;
+
+        setSelectedMovie(id);
+        const target = e.currentTarget as HTMLLIElement;
+        const targetInfo = target.getElementsByClassName("movie__info-container")[0] as HTMLDivElement;
+
+        setTimeout(() => {
+            lastMovie?.style.removeProperty("height");
+            target.style.height = target.offsetHeight + targetInfo.offsetHeight + 16 + 'px';
+        }, 0);
+
+        setLastMovie(target);
+    }
+
+    const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.value === '') {
+            setFilteredMovies(movies);
+            setSearchQuery('');
+            return;
+        }
+
+        setSearchQuery(e.target.value);
+        setGenre(-1);
+        setFilteredMovies(filteredMovies.filter(movie => movie.title.toLowerCase().includes(e.target.value.toLowerCase())));
+    }
+
+    useEffect(() => {
+        getGenres();
+        getMovies();
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+    useEffect(() => {
+        if (isLoadTriggerVisible) {
+            getMovies();
+        }
+    }, [isLoadTriggerVisible]) // eslint-disable-line react-hooks/exhaustive-deps
+
+    if (error) {
+        return <div>Error {error.status_code}: {error.status_message}</div>;
+    }
+
+    if (!isLoaded) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div className="app">
+            <header className="header">
+                <h1>
+                    Popular movies
+                </h1>
+                <div className="input-wrapper">
+                    <input type="text" placeholder="Search for movies..." value={searchQuery}
+                           onChange={handleSearchChange}/>
+                    <span className="material-icons">search</span>
+                </div>
+                <ul className={`genres ${showAllGenres ? 'genres__showing-all' : ''}`} role="tablist"
+                    aria-label="Genres">
+                    {genres.slice(0, showAllGenres ? genres.length : 6).map(genre =>
+                        <li className={`genres__genre ${selectedGenre === genre.id ? 'genres__genre--selected' : ''}`}
+                            aria-selected={selectedGenre === genre.id}
+                            key={genre.id}
+                            role="tab"
+                            onClick={() => setGenre(genre.id)}
+                        >
+                            {genre.name}
+                        </li>
+                    )}
+                    <li className="genres__show-all" onClick={() => setShowAllGenres(!showAllGenres)}>
+                        {showAllGenres ? 'Less' : 'More'}
+                    </li>
+                </ul>
+            </header>
+            <main className="main">
+                <ul className="movie-list">
+                    {filteredMovies.map(movie =>
+                        <li className={`movie ${movie.id === selectedMovie ? "movie--selected" : ""}`}
+                            onClick={(e) => setMovie(e, movie.id)}
+                            key={movie.id}
+                        >
+                            <div className="movie__poster">
+                                <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} draggable="false"
+                                     alt={movie.title}/>
+                            </div>
+                            <div className="movie__info-container">
+                                <div className="movie__gradient"></div>
+                                <div className="movie__info">
+                                    <h2 className="movie__title">{movie.title}</h2>
+                                    <div className="movie__rating"
+                                         aria-label={"Rating: " + movie.vote_average + "out of 10"}>
+                                        {[...Array(5)].map((_, i) =>
+                                                <span className="material-icons movie__rating" aria-hidden="true" key={i}>
+                                        {i < movie.vote_average / 2 ? 'star' :
+                                            i - 0.5 < movie.vote_average / 2 ? 'star_half' : 'star_border'}
+                                    </span>
+                                        )}
+                                    </div>
+                                    <p>
+                                        {movie.overview}
+                                    </p>
+                                    <div className="movie__tags">
+                                        {movie.genre_ids.map(genreId =>
+                                                <span className="movie__tag" onClick={() => setGenre(genreId)}
+                                                      key={genreId}>
+                                            #{genres.find(genre => genre.id === genreId)?.name}
+                                        </span>
+                                        )}
+                                    </div>
+                                </div>
+                                <img className="movie__backdrop" draggable="false"
+                                     src={`https://image.tmdb.org/t/p/w780/${movie.backdrop_path}`} alt={movie.title}/>
+                            </div>
+                        </li>
+                    )}
+                    <li className="movie__load-trigger" ref={loadTriggerRef} aria-hidden="true"></li>
+                </ul>
+            </main>
+            <footer></footer>
+        </div>
+    );
 }
 
 export default App;
